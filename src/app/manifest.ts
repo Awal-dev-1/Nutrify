b6d2f2e6e06c2ecf630f4fddc9cb88c832ec8582
Awal Mohammed <mohammedwal0012@gmail.com>
2026-04-03 08:46:49 +0000

    we are unable to download the the app when we lunch the web on our phone

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -12,6 +12,11 @@ export default function manifest(): MetadataRoute.Manifest {
     theme_color: '#00b371',
     icons: [
       {
+        src: '/favicon.ico',
+        sizes: 'any',
+        type: 'image/x-icon',
+      },
+      {
         src: '/icon-192x192.png',
         sizes: '192x192',
         type: 'image/png',
