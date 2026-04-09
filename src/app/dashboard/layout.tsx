30a43a0a8c7a0c259cd5e6f303d2d009931cfe91
Awal Mohammed <mohammedwal0012@gmail.com>
2026-04-09 23:26:32 +0000

    Fix the mobile scrolling issue caused by the header (greeting + theme to

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -84,7 +84,7 @@ export default function DashboardLayout({
       <SidebarInset>
         <DashboardHeader />
         <motion.div
-          className="bg-background px-5 pt-2 pb-20 md:p-6 md:pt-6 min-h-[101vh]"
+          className="bg-background px-5 pt-4 pb-20 md:p-6 md:pb-6 min-h-[101vh]"
           initial={{ opacity: 0, y: 20 }}
           animate={{ opacity: 1, y: 0 }}
           transition={{ ease: "easeInOut", duration: 0.2 }}
