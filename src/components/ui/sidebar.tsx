793266a9a0c941c43d08e3be12b1212b428a909e
Awal Mohammed <mohammedwal0012@gmail.com>
2026-04-10 10:28:53 +0000

    we should get scroll bar on the landing page only when on desktop

--- src/components/ui/sidebar.tsx
+++ src/components/ui/sidebar.tsx
@@ -332,7 +332,7 @@ const SidebarInset = React.forwardRef<
     <main
       ref={ref}
       className={cn(
-        "relative flex-1 bg-background",
+        "relative flex-1 bg-background overflow-y-auto no-scrollbar",
         "md:peer-data-[variant=inset]:m-2 md:peer-data-[state=collapsed]:peer-data-[variant=inset]:ml-2 md:peer-data-[variant=inset]:ml-0 md:peer-data-[variant=inset]:rounded-xl md:peer-data-[variant=inset]:shadow",
         className
       )}
